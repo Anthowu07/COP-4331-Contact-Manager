9dc61decf1ad7141e13c52a72e4559c60551104e
Anthowu07 <anthony07wu@gmail.com>
2023-05-30 15:27:20 -0400

    add registration results

--- js/code.js
+++ js/code.js
@@ -161,62 +161,62 @@ function validate() {
   }
 
   function registerNewUser()
+{
+	let newFirstName = document.getElementById("newFirstName").value;
+	let newLastName = document.getElementById("newLastName").value;
+	let newLogin = document.getElementById("newLogin").value;
+	let newPassword = document.getElementById("newPassword").value;
+	document.getElementById("registerResult").innerHTML = "";
+
+  // Prevent empty credentials from being entered
+  if (newLogin == "" || newPassword == "" || newFirstName == "" || newLastName == "")
   {
-	  let newFirstName = document.getElementById("newFirstName").value;
-	  let newLastName = document.getElementById("newLastName").value;
-	  let newLogin = document.getElementById("newLogin").value;
-	  let newPassword = document.getElementById("newPassword").value;
-	  document.getElementById("registerResult").innerHTML = "";
-  
-	// Prevent empty credentials from being entered
-	if (newLogin == "" || newPassword == "")
-	{
-	  document.getElementById("registerResult").innerHTML = "Invalid credentials";
-	  return;
-	}
-  
-	  let tmp = {
-		  firstName:newFirstName,
-		  lastName:newLastName,
-		  login:newLogin,
-		  password:newPassword
-	  };
-  
-	  let jsonPayload = JSON.stringify( tmp );
-	  let url = urlBase + '/RegisterNewUser.' + extension;
-	  
-	  let xhr = new XMLHttpRequest();
-	  xhr.open("POST", url, true);
-	  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	  try
-	  {
-		  xhr.onreadystatechange = function() 
-		  {
-			  if (this.readyState == 4 && this.status == 200) 
-			  {
-		  let jsonObject = JSON.parse( xhr.responseText );
-  
-		  const errorString = "User already exists";
-		  let result = errorString.localeCompare(jsonObject.error);
-  
-		  if (result == 1) 
-		  {
-			document.getElementById("registerResult").innerHTML = "User already exists";
-			return;
-		  }
-  
-		  // I'm not sure how to make this play nice with the required field process
-				  document.getElementById("registerResult").innerHTML = "New User has been registered!";
-			  }
-		  };
-		  xhr.send(jsonPayload);
-	  }
-	  catch (err)
-	  {
-		  document.getElementById("registerResult").innerHTML = err.message;
-	  }
-  
+    document.getElementById("registerResult").innerHTML = "Error, one or more fields are empty.";
+    return;
   }
+
+	let tmp = {
+		firstName:newFirstName,
+		lastName:newLastName,
+		login:newLogin,
+		password:newPassword
+	};
+
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/RegisterNewUser.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+        let jsonObject = JSON.parse( xhr.responseText );
+
+        const errorString = "User already exists";
+        let result = errorString.localeCompare(jsonObject.error);
+
+        if (result == 1) 
+        {
+          document.getElementById("registerResult").innerHTML = "User already exists";
+          return;
+        }
+
+        // I'm not sure how to make this play nice with the required field process
+				document.getElementById("registerResult").innerHTML = "New User has been registered!";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch (err)
+	{
+		document.getElementById("registerResult").innerHTML = err.message;
+	}
+}
 
 function searchContacts()
 {
